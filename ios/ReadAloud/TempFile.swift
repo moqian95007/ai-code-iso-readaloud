20573c7a18066f69a5d88529d441f019d40c6aeb
blue <blue.liu@waymap.cn>
2025-06-04 13:43:54 +0800

    清理一些无效文件

--- ios/ReadAloud/TempFile.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Combine
-import StoreKit
-import SwiftUI
-
-#if canImport(StoreKit) && compiler(>=5.5)
-@available(iOS 15.0, *)
-typealias TransactionAPI = StoreKit.Transaction
-#endif
